9d26f3142001b912a6acec09ec5bd9b309097b3b
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2013-09-02 14:55:54 +0200

    lib: Add WinIO version 3 support on Windows

--- lib/i386-io-windows.h
+++ lib/i386-io-windows.h
@@ -1,62 +1,86 @@
 /*
  *	The PCI Library -- Access to i386 I/O ports on Windows
  *
- *	Copyright (c) 2004 Alexander Stock <stock.alexander@gmx.de>
- *	Copyright (c) 2006 Martin Mares <mj@ucw.cz>
+ *  	Copyright (c) 2004 	Alexander Stock <stock.alexander@gmx.de>
+ *  	Copyright (c) 2006 	Martin Mares <mj@ucw.cz> 
+ *	Copyright (c) 2011-2012 Jernej Simončič <jernej+s-pciutils@eternallybored.org>
+ *	Copyright (c) 2013      Samuel Pitoiset   <samuel.pitoiset@gmail.com>
  *
  *	Can be freely distributed and used under the terms of the GNU GPL.
+ *
+ * Changelog:
+ * 2013-08-20: remove useless support of DirectIO
+ * 2012-02-19: added back support for WinIo when DirectIO isn't available
+ * 2011-06-20: original release for DirectIO library
  */
 
-#include <io.h>
+#define WIN32_LEAN_AND_MEAN
+#define NOGDI
 #include <windows.h>
 
-#ifndef __GNUC__
-#include <conio.h>
-#else
-int _outp(unsigned short port, int databyte);
-unsigned short _outpw(unsigned short port, unsigned short dataword);
-unsigned long _outpd(unsigned short port, unsigned long dataword);
-int _inp(unsigned short port);
-unsigned short _inpw(unsigned short port);
-unsigned long _inpd(unsigned short port);
-#endif
+typedef BOOL (WINAPI *SETDLLDIRECTORY)(LPCSTR);
 
-#define outb(x,y) _outp(y,x)
-#define outw(x,y) _outpw(y,x)
-#define outl(x,y) _outpd(y,x)
+#define LIB_DIRECTIO 1
+#define LIB_WINIO 2
+int lib_used;
 
-#define inb(x) _inp(x)
-#define inw(x) _inpw(x)
-#define inl(x) _inpd(x)
+/* WinIo declarations */
+typedef BYTE bool;
+
+typedef bool (_stdcall* INITIALIZEWINIO)(void);
+typedef void (_stdcall* SHUTDOWNWINIO)(void);
+typedef bool (_stdcall* GETPORTVAL)(WORD,PDWORD,BYTE);
+typedef bool (_stdcall* SETPORTVAL)(WORD,DWORD,BYTE);
+
+SHUTDOWNWINIO ShutdownWinIo;
+GETPORTVAL GetPortVal;
+SETPORTVAL SetPortVal;
+INITIALIZEWINIO InitializeWinIo;
 
 static int
 intel_setup_io(struct pci_access *a)
 {
-  typedef int (*MYPROC)(void);
-  MYPROC InitializeWinIo;
   HMODULE lib;
 
-  lib = LoadLibrary("WinIo.dll");
-  if (!lib)
-    {
-      a->warning("i386-io-windows: Couldn't load WinIo.dll.");
-      return 0;
-    }
-  /* XXX: Is this really needed? --mj */
-  GetProcAddress(lib, "InitializeWinIo");
+  SETDLLDIRECTORY fnSetDllDirectory;
 
-  InitializeWinIo = (MYPROC) GetProcAddress(lib, "InitializeWinIo");
-  if (!InitializeWinIo)
-    {
-      a->warning("i386-io-windows: Couldn't find InitializeWinIo function.");
-      return 0;
-    }
+  /* remove current directory from DLL search path */
+  fnSetDllDirectory = GetProcAddress(GetModuleHandle("kernel32"), "SetDllDirectoryA");
+  if (fnSetDllDirectory)
+    fnSetDllDirectory("");
 
-  if (!InitializeWinIo())
-    {
-      a->warning("i386-io-windows: InitializeWinIo() failed.");
+  if ((GetVersion() & 0x80000000) == 0) {
+    /* running on NT, try WinIo version 3 (32 or 64 bits) */
+#ifdef WIN64
+    lib = LoadLibrary("WinIo64.dll");
+#else
+    lib = LoadLibrary("WinIo32.dll");
+#endif
+  }
+
+  if (!lib) {
+    a->warning("i386-io-windows: Failed to load WinIo library.\n");
+    return 0;
+  }
+
+#define GETPROC(n, d) 							\
+  n = (d) GetProcAddress(lib, #n); 					\
+  if (!n) { 								\
+    a->warning("i386-io-windows: Failed to load " #n " function.\n"); 	\
+    return 0; 								\
+  }
+
+  GETPROC(InitializeWinIo, INITIALIZEWINIO);
+  GETPROC(ShutdownWinIo, SHUTDOWNWINIO);
+  GETPROC(GetPortVal, GETPORTVAL);
+  GETPROC(SetPortVal, SETPORTVAL);
+
+#undef GETPROC
+
+  if (!InitializeWinIo()) {
+      a->warning("i386-io-windows: Failed to initialize WinIo.\n");
       return 0;
-    }
+  }
 
   return 1;
 }
@@ -64,6 +88,54 @@ intel_setup_io(struct pci_access *a)
 static inline int
 intel_cleanup_io(struct pci_access *a UNUSED)
 {
-  //TODO: DeInitializeWinIo!
+  ShutdownWinIo();
   return 1;
+}
+
+static inline u8
+inb(u16 port)
+{
+  DWORD pv;
+
+  if (GetPortVal(port, &pv, 1))
+    return (u8)pv;
+  return 0;
+}
+
+static inline u16
+inw(u16 port)
+{
+  DWORD pv;
+  
+  if (GetPortVal(port, &pv, 2))
+    return (u16)pv;
+  return 0;
+}
+
+static inline u32
+inl(u16 port)
+{
+  DWORD pv;
+
+  if (GetPortVal(port, &pv, 4))
+    return (u32)pv;
+  return 0;
+}
+
+static inline void
+outb(u8 value, u16 port)
+{
+  SetPortVal(port, value, 1);
+}
+
+static inline void
+outw(u16 value, u16 port)
+{
+  SetPortVal(port, value, 2);
+}
+
+static inline void
+outl(u32 value, u16 port)
+{
+  SetPortVal(port, value, 4);
 }
